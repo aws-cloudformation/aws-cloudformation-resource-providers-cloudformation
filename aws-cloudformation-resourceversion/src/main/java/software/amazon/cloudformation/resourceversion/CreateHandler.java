5d8c66445865b2a997221ddf38c9428318820417
Ryan Lohan <ryanloha@amazon.com>
2020-06-19 11:12:53 -0700

    Removed commented out block

--- aws-cloudformation-resourceversion/src/main/java/software/amazon/cloudformation/resourceversion/CreateHandler.java
+++ aws-cloudformation-resourceversion/src/main/java/software/amazon/cloudformation/resourceversion/CreateHandler.java
@@ -40,69 +40,6 @@ public class CreateHandler extends BaseHandlerStd {
             .then(progress -> new ReadHandler().handleRequest(proxy, request, progress.getCallbackContext(), proxyClient, logger));
     }
 
-//    /**
-//     * To ensure we can return a primaryIdentifier for this type within the 60s timeout required,
-//     * we must predict the Arn which this type will be registered with. There may be some edge case
-//     * issues here, which we can fix by moving this prediction to the Registry Service itself
-//     */
-//    ProgressEvent<ResourceModel, CallbackContext> predictArn(
-//        final ProxyClient<CloudFormationClient> proxyClient,
-//        final ResourceHandlerRequest<ResourceModel> request,
-//        final CallbackContext callbackContext) {
-//
-//        String arn;
-//        try {
-//            // find the highest existing version number for this type
-//            int highestExistingVersionNumber = 0;
-//            while (true) {
-//                final ListTypeVersionsRequest listTypeVersionsRequest = ListTypeVersionsRequest.builder()
-//                    .type(RegistryType.RESOURCE)
-//                    .typeName(request.getDesiredResourceState().getTypeName())
-//                    .build();
-//                final ListTypeVersionsResponse response = proxyClient.injectCredentialsAndInvokeV2(listTypeVersionsRequest,
-//                    proxyClient.client()::listTypeVersions);
-//
-//                final Optional<Integer> highestVersionInSet = response.typeVersionSummaries()
-//                    .stream()
-//                    .map(t -> Integer.parseInt(t.arn().substring(t.arn().lastIndexOf("/") + 1)))
-//                    .max(Integer::compareTo);
-//
-//                highestExistingVersionNumber = Math.max(highestExistingVersionNumber, highestVersionInSet.get());
-//
-//                if (response.nextToken() == null) {
-//                    // capture an Arn - they are all the same except the version Id which we'll replace ouside the loop
-//                    arn = response.typeVersionSummaries().get(0).arn();
-//                    break;
-//                }
-//            }
-//
-//            // bump the version number for this revision
-//            arn = arn
-//                .substring(0, arn.lastIndexOf("/") + 1)
-//                .concat(String.format("%08d", highestExistingVersionNumber + 1));
-//
-//        } catch (final TypeNotFoundException e) {
-//            // registration can be assumed to be the first version for a type
-//            arn = String.format("arn:%s:cloudformation:%s:%s:type/resource/%s/00000001",
-//                request.getAwsPartition(),
-//                request.getRegion(),
-//                request.getAwsAccountId(),
-//                request.getDesiredResourceState().getTypeName().replace("::", "-"));
-//        } catch (final CfnRegistryException e) {
-//            logger.log(Arrays.toString(e.getStackTrace()));
-//            throw e;
-//        }
-//
-//        callbackContext.setPredictedArn(arn);
-//        request.getDesiredResourceState().setArn(arn);
-//
-//        return ProgressEvent.<ResourceModel, CallbackContext>builder()
-//            .callbackContext(callbackContext)
-//            .status(OperationStatus.IN_PROGRESS)
-//            .resourceModel(request.getDesiredResourceState())
-//            .build();
-//    }
-
     /**
      * To ensure we can return a primaryIdentifier for this type within the 60s timeout required,
      * we must predict the Arn which this type will be registered with. There may be some edge case
