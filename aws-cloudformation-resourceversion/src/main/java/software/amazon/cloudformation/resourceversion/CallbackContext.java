6256056e561e44a8fa25dfd805c14eb90df2e347
uma gudipalli <ugudip@amazon.com>
2021-01-15 15:35:55 -0800

    Remove unnecessary variables in CallBackContext

--- aws-cloudformation-resourceversion/src/main/java/software/amazon/cloudformation/resourceversion/CallbackContext.java
+++ aws-cloudformation-resourceversion/src/main/java/software/amazon/cloudformation/resourceversion/CallbackContext.java
@@ -1,6 +1,5 @@
 package software.amazon.cloudformation.resourceversion;
 
-import software.amazon.awssdk.services.cloudformation.model.DeprecatedStatus;
 import software.amazon.cloudformation.proxy.StdCallbackContext;
 
 @lombok.Getter
@@ -12,5 +11,4 @@ import software.amazon.cloudformation.proxy.StdCallbackContext;
 @lombok.NoArgsConstructor
 public class CallbackContext extends StdCallbackContext {
     private String registrationToken;
-    private DeprecatedStatus deprecatedStatus;
 }
